748910d8c0ad1d1a53dffec74f5818d0cb52bc02
Karol Trombala <ktrombala@gmail.com>
2023-05-25 21:17:59 +0200

    task-05
    update

---
+++ js/task-05.js
@@ -0,0 +1,6 @@
+const inputName = document.getElementById ("name-input");
+const outputName = document.getElementById ("name-output");
+
+inputName.addEventListener ("input", () => {
+    outputName.textContent = inputName.value ? inputName.value: "Anonymous";
+})
