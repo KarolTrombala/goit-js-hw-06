ba83612d6a750fa8a1394b4d2aa5699c5a460fa9
Karol Trombala <ktrombala@gmail.com>
2023-05-28 21:02:36 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector(".login-form");
 
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = e.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Uzupełnij wszystkie pola !!!");
+  }
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+
+  e.currentTarget.reset();
+};
+
+formEl.addEventListener("submit", handleSubmit);
