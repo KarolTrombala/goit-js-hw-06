24a2a7e540119f431412a601360b7921384c8e96
Karol Trombala <ktrombala@gmail.com>
2023-06-06 21:00:27 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const inputEl = document.getElementById("font-size-control");
 const textEl = document.getElementById("text");
 
-inputEl.addEventListener("input", () => {
-  textEl.style.fontSize = `${inputEl.value}px`;
+inputEl.addEventListener("input", (e) => {
+  textEl.style.fontSize = `${e.currentTarget.value}px`;
 });
